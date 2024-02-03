7a94b3f588ca4da147db7f42beed6c16fe8b85e1
acpmasquerade <dhrubastergmail.com>
2024-02-03 23:43:27 +0545

    locally resolving if a boolean is set

--- scripts/python/validate.py
+++ scripts/python/validate.py
@@ -1,18 +1,41 @@
 import jsonschema
+import referencing
 import json
+import os
+import requests
+import referencing.retrieval
+
+RESOLVE_LOCALLY = True
 
 # Define the schema
-schema = json.load(open("../../specs/v1/master.schema.json"))
+base_dir = os.path.dirname(os.path.abspath(__file__))
+schema_file = "../../specs/v1/master.schema.json"
+json_file = "../../examples/sample.json"
 
-# Define the JSON data
-data = json.load(open("../../examples/sample.json"))
+schema = json.load(open(os.path.join(base_dir, schema_file)))
+data = json.load(open(os.path.join(base_dir, json_file)))
+
+# remove the $schema key from the data to avoid validation error
+data.pop("$schema", None)
 
 print("Schema: ", schema)
 print("Data: ", data)
 
-# Validate the data
+from referencing import Registry
+from referencing.typing import URI
+import referencing.retrieval
+
+@referencing.retrieval.to_cached_resource()
+def mno_retreiver(url):
+    if RESOLVE_LOCALLY is True and url.startswith("https://standards.mnoforms.com/"):
+        url = url.replace("https://standards.mnoforms.com/", "http://standards.mnoforms.local/")
+
+    resp = requests.get(url)
+    return resp.text
+
 try:
-    jsonschema.validate(instance=data, schema=schema)
+    schema_registry = referencing.Registry(retrieve=mno_retreiver)
+    jsonschema.validate(instance=data, schema=schema, registry=schema_registry)
     print("JSON data is valid")
 except jsonschema.exceptions.ValidationError as ve:
     print("JSON data is invalid", ve)
