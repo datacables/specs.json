e55a5b410017bd2546494437b3d4bb79cba7db59
acpmasquerade <dhrubastergmail.com>
2024-02-04 00:04:25 +0545

    sample validator validated

--- scripts/python/validate.py
+++ scripts/python/validate.py
@@ -1,16 +1,28 @@
-import jsonschema
-import referencing
 import json
 import os
-import requests
+
+import jsonschema
+import referencing
 import referencing.retrieval
+import requests
+from referencing import Registry
+from referencing.typing import URI
 
 RESOLVE_LOCALLY = True
+schema_file = "../../specs/v1/master.schema.json"
+json_file = "../../examples/sample.json"
+
+@referencing.retrieval.to_cached_resource()
+def mno_retreiver(url):
+    if RESOLVE_LOCALLY is True and url.startswith("https://standards.mnoforms.com/"):
+        url = url.replace("https://standards.mnoforms.com/", "http://standards.mnoforms.local/")
+
+    resp = requests.get(url)
+    return resp.text
+
 
 # Define the schema
 base_dir = os.path.dirname(os.path.abspath(__file__))
-schema_file = "../../specs/v1/master.schema.json"
-json_file = "../../examples/sample.json"
 
 schema = json.load(open(os.path.join(base_dir, schema_file)))
 data = json.load(open(os.path.join(base_dir, json_file)))
@@ -20,18 +32,6 @@ data.pop("$schema", None)
 
 print("Schema: ", schema)
 print("Data: ", data)
-
-from referencing import Registry
-from referencing.typing import URI
-import referencing.retrieval
-
-@referencing.retrieval.to_cached_resource()
-def mno_retreiver(url):
-    if RESOLVE_LOCALLY is True and url.startswith("https://standards.mnoforms.com/"):
-        url = url.replace("https://standards.mnoforms.com/", "http://standards.mnoforms.local/")
-
-    resp = requests.get(url)
-    return resp.text
 
 try:
     schema_registry = referencing.Registry(retrieve=mno_retreiver)
